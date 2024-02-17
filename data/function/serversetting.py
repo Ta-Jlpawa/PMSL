7fc6214a3b4f2b5ec2ebc6d02d8b8b045a8bf8f9
Ta-Jlpawa <114741206+Ta-Jlpawa@users.noreply.github.com>
2024-02-17 19:08:23 +0800

    Delete data directory

--- data/function/serversetting.py
+++
@@ -1,7 +0,0 @@
-
-'''
-
-快捷更改服务器配置文件专用模块,在主程序中使用 import data.function.serversetting 来导入.
-此模块需要编写
-
-'''
--- data/future/tkui.py
+++
@@ -1,5 +0,0 @@
-import tkinter
-from tkinter import messagebox
-def tkui(a):
-    if str(a) == 'unknown set':
-        messagebox.askokcancel('Your set ERROR')
